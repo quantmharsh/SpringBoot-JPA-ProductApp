b7ddbda4317bb065b8abfb857cdb63a4dc4393ae
Harsh Srivastava <srivastavah240@gmail.com>
2025-08-08 23:55:15 +0530

    (Feat):Worked with  All 3 Dependeny Injection for @AutoWired

--- src/main/java/com/harsh/ProductApp/Dev.java
+++ src/main/java/com/harsh/ProductApp/Dev.java
@@ -1,10 +1,30 @@
 package com.harsh.ProductApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 //adding component annotation to tell IOC container to create the  object of this class
 @Component
 public class Dev {
+     // 3 Ways of using Autowired Dependency Injection
+    // 1. Field Injection
+//    @Autowired //Connect Dev Component with Laptop Component without need to create object of laptop
+//    private  Laptop laptop;
+    private Laptop laptop;
+
+    // 2.Constructor Injection
+//    public Dev(Laptop laptop)
+//    {
+//        this.laptop=laptop;
+//    }
+    // 3. Setter Injection
+    @Autowired
+    public  void GetLaptop(Laptop laptop){
+        laptop.Compile();
+    }
+
+
+
 
     public void build(){
         System.out.println("Building Springboot application");
---
+++ src/main/java/com/harsh/ProductApp/Laptop.java
@@ -0,0 +1,13 @@
+package com.harsh.ProductApp;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void Compile(){
+        System.out.println("Compiling code with 404 Bugs");
+
+    }
+}
